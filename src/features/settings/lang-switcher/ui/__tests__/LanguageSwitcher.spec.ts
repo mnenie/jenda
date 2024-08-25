ab6f6a456bfe4c4a4221ff58e2bacadcba0786d0
mnenie <121057011+mneniee@users.noreply.github.com>
2024-08-25 23:00:22 +0300

    test: add tests for LanguageSwitcher

---
+++ src/features/settings/lang-switcher/ui/__tests__/LanguageSwitcher.spec.ts
@@ -0,0 +1,64 @@
+import { mount } from '@vue/test-utils';
+import { describe, it, expect, vi } from 'vitest';
+import LanguageSwitcher from '../LanguageSwitcher.vue';
+import i18n from '@/shared/lib/i18n';
+import { nextTick, ref, shallowReactive } from 'vue';
+import { useLanguage } from '@/shared/lib/composables';
+import { useI18n } from 'vue-i18n';
+
+vi.mock('@vueuse/integrations/useCookies', () => {
+  return {
+    useCookies: () => ({
+      get(key: string) {
+        return key === 'i18n' ? 'en-US' : undefined;
+      }
+    })
+  };
+});
+
+vi.mock('vue-i18n', async (importOriginal) => {
+  const actual = await importOriginal();
+  return {
+    ...(actual as unknown as object),
+    useI18n: () => ({
+      locale: ref('en-US')
+    })
+  };
+});
+
+describe('tests for LanguageSwitcher.vue', () => {
+  const wrapper = mount(LanguageSwitcher, {
+    global: {
+      plugins: [i18n]
+    }
+  });
+
+  it('should render correctly', () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+  it('should render subcomponents', () => {
+    expect(wrapper.findComponent({ name: 'UiSelect' }).exists()).toBe(true);
+  });
+});
+
+describe('useLanguage composable tests', () => {
+  // mocks
+  const options = shallowReactive([
+    { name: 'English', value: 'en-US' },
+    { name: 'Русский', value: 'ru-RU' },
+    { name: '简体中文', value: 'zh-CN' }
+  ]);
+
+  it('should correctly sync language with `locale`', async () => {
+    const { locale } = useI18n();
+    const language = ref('');
+
+    useLanguage(options, language);
+
+    expect(language.value).toBe('English');
+
+    locale.value = 'ru-RU';
+    await nextTick();
+    expect(locale.value).toBe('ru-RU');
+  });
+});
