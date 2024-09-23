602f8d9ec68250e27cfdfe6f437849642cd8c5d6
mnenie <1alexpeshkov@gmail.com>
2024-09-24 00:38:53 +0300

    feat: add visibility in watcher

--- src/features/boards/charts/model/composables/useChart.ts
+++ src/features/boards/charts/model/composables/useChart.ts
@@ -1,4 +1,4 @@
-import { computed, effectScope, onScopeDispose, watchEffect } from 'vue';
+import { computed, effectScope, onScopeDispose, watch } from 'vue';
 import { useDark } from '@vueuse/core';
 import type { ChartData, ChartOptions } from 'chart.js';
 
@@ -15,16 +15,20 @@ export default function useChart(data: ChartData, options: ChartOptions) {
       return isDark.value ? 'rgb(163 163 163)' : 'rgb(82 82 82)';
     });
 
-    watchEffect(() => {
-      data.datasets[0].backgroundColor = bgColor.value;
-      data.datasets[0].borderColor = bgColor.value;
-      options.scales!.x!.ticks!.color = tickColor.value;
-      options.scales!.y!.ticks!.color = tickColor.value;
-      // @ts-expect-error missing type
-      options.scales!.y!.border!.color = bgColor.value;
-      // @ts-expect-error missing type
-      options.scales!.x!.border!.color = bgColor.value;
-    });
+    watch(
+      [bgColor, tickColor],
+      ([bg, tick], _) => {
+        data.datasets[0].backgroundColor = bg;
+        data.datasets[0].borderColor = bg;
+        options.scales!.x!.ticks!.color = tick;
+        options.scales!.y!.ticks!.color = tick;
+        // @ts-expect-error missing type
+        options.scales!.y!.border!.color = bg;
+        // @ts-expect-error missing type
+        options.scales!.x!.border!.color = bg;
+      },
+      { immediate: true }
+    );
   });
 
   onScopeDispose(() => {
