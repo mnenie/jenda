cd9cfa50e0c196407ea5eed49ca542da78186e13
mnenie <1alexpeshkov@gmail.com>
2024-12-04 14:38:30 +0300

    feat: add unocss config

---
+++ core/client/uno.config.ts
@@ -0,0 +1,24 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+} from 'unocss'
+
+export default defineConfig({
+  shortcuts: [
+    // TODO
+  ],
+  presets: [
+    presetAttributify(),
+    presetUno(),
+    presetIcons({
+      extraProperties: {
+        'display': 'inline-block',
+        'vertical-align': 'middle',
+      },
+    }),
+    presetTypography(),
+  ],
+})
--- core/client/vite.config.ts
+++ core/client/vite.config.ts
@@ -1,13 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
+import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), svgLoader()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    UnoCSS(),
+    svgLoader(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
