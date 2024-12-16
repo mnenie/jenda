d98105b80919e6a126853d6d7763877eb5c65829
mnenie <1alexpeshkov@gmail.com>
2024-12-16 17:01:05 +0300

    fix: review composable

--- core/client/src/features/auth/model/composables/useTextChanging.ts
+++ core/client/src/features/auth/model/composables/useTextChanging.ts
@@ -1,16 +1,18 @@
 import type { Review } from '../types'
-import { onMounted, ref } from 'vue'
+import { ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
 
 export default function useTextChanging(reviews: Review[]) {
   // so, maybe Ill do with live data
   const currentIndex = ref(0)
+  const route = useRoute()
 
   const changeReviewText = () => {
     currentIndex.value = Math.floor(Math.random() * reviews.length)
   }
-  onMounted(() => {
+  watch(() => route.path, () => {
     changeReviewText()
-  })
+  }, { immediate: true })
 
   return {
     currentIndex,
