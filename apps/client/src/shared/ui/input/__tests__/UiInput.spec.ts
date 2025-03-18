4ae0470f7160c07a30d937bcbfb8b006e52a9c92
mnenie <121057011+mneniee@users.noreply.github.com>
2025-03-18 19:31:01 +0300

    chore: update input spec

--- apps/client/src/shared/ui/input/__tests__/UiInput.spec.ts
+++ apps/client/src/shared/ui/input/__tests__/UiInput.spec.ts
@@ -1,6 +1,8 @@
+import { nextTick } from 'vue'
 import { mount } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
 import UiInput from '../UiInput.vue'
+import type { InputVariants } from '..'
 
 describe('tests for UiInput', () => {
   const wrapper = mount(UiInput, {
@@ -19,5 +21,20 @@ describe('tests for UiInput', () => {
     const input = wrapper.find('input')
     await input.setValue('new')
     expect(wrapper.emitted('update:modelValue')![0]).toEqual(['new'])
+  })
+
+  it('should render variant correctly', async () => {
+    const variants = [
+      'default',
+      'filled',
+    ] as InputVariants['variant'][]
+    const input = wrapper.find('input')
+    expect(input.exists()).toBe(true)
+
+    for (const variant of variants) {
+      await wrapper.setProps({ variant })
+      await nextTick()
+      expect(input.classes()).toContain(`input-${variant}`)
+    }
   })
 })
