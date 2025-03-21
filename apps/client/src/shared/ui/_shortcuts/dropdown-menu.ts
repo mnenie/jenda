6cf0240718ac8a328ef4b3e9d476a0e54e671b7c
mnenie <121057011+mneniee@users.noreply.github.com>
2025-03-21 21:53:54 +0300

    chore(ui): update alert and dropdown trigger

--- apps/client/src/shared/ui/_shortcuts/dropdown-menu.ts
+++ apps/client/src/shared/ui/_shortcuts/dropdown-menu.ts
@@ -10,7 +10,8 @@ export const staticDropdownMenu: Record<`${DropdownMenuPrefix}-${string}` | Drop
   'dropdown-menu-separator': '-mx-1 my-1 h-px bg-neutral-100 dark:bg-neutral-700/40',
   'dropdown-menu-shortcut': 'ml-auto text-xs tracking-widest opacity-60',
   'dropdown-menu-sub-content': 'z-50 min-w-32 overflow-hidden rounded-md border border-neutral-200 bg-white p-1 text-neutral-950 shadow-lg dark:(border-neutral-700/40 bg-neutral-800 text-neutral-50)',
-  'dropdown-menu-sub-trigger': 'flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-default outline-none focus:bg-neutral-100 data-[state=open]:bg-neutral-100 dark:(focus:bg-neutral-800 data-[state=open]:bg-neutral-800)',
+  'dropdown-menu-sub-trigger': 'flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-default outline-none focus:bg-neutral-100 data-[state=open]:bg-neutral-100 dark:(focus:bg-neutral-700/40 data-[state=open]:bg-neutral-700/40)',
+  'dropdown-menu-sub-trigger-icon': 'ml-auto h-4 w-4 shrink-0 text-neutral-500 dark:text-neutral-400',
   'dropdown-menu-trigger': 'outline-none',
 }
 
--- apps/client/src/shared/ui/alert/UiAlert.vue
+++ apps/client/src/shared/ui/alert/UiAlert.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { computed, type HTMLAttributes, ref } from 'vue'
+import { computed, type HTMLAttributes } from 'vue'
+import { createReusableTemplate } from '@vueuse/core'
 import { alertVariants, type AlertVariants } from '.'
 import { cn } from '@/shared/libs/shadcn/utils'
 
@@ -7,36 +8,73 @@ interface AlertProps {
   variant?: AlertVariants['variant']
   closable?: boolean
   content?: string
+  rp?: string
+  withAnimation?: boolean
   class?: HTMLAttributes['class']
 }
 
-const { variant = 'default', closable, content, class: alertClass } = defineProps<AlertProps>()
+const { variant = 'default', class: alertClass, ...props } = defineProps<AlertProps>()
 
 const emits = defineEmits(['close'])
 
-const isAlertVisible = ref(true)
+const isAlertVisible = defineModel<boolean>('open', {
+  default: true,
+})
 
-const valueOfRightPadding = computed(() => (closable ? '32px' : '10px'))
+const valueOfRightPadding = computed(() => (props.rp ? '' : props.closable ? '32px' : '10px'))
+
+function closeAlert() {
+  isAlertVisible.value = false
+  emits('close')
+}
+
+const [DefineTemplate, ReuseTemplate] = createReusableTemplate()
 </script>
 
 <template>
-  <div
-    v-if="isAlertVisible"
-    :class="cn(
-      'alert',
-      alertVariants({ variant }),
-      alertClass,
-    )"
-    :style="{ 'padding-right': valueOfRightPadding }"
-  >
-    <slot v-if="$slots.default || content">
-      <span class="text-default">{{ content }}</span>
-    </slot>
+  <DefineTemplate>
     <div
-      v-if="closable"
-      i-lucide-x
-      class="alert-close"
-      @click="isAlertVisible = false, emits('close')"
-    />
-  </div>
+      v-if="isAlertVisible"
+      :class="cn(
+        'alert',
+        alertVariants({ variant }),
+        alertClass,
+      )"
+      :style="{ 'padding-right': valueOfRightPadding }"
+    >
+      <slot v-if="$slots.default || content">
+        <span class="text-default">{{ content }}</span>
+      </slot>
+      <div
+        v-if="closable"
+        i-lucide-x
+        class="alert-close"
+        @click="closeAlert"
+      />
+    </div>
+  </DefineTemplate>
+  <Transition v-if="withAnimation" name="slide-fade">
+    <ReuseTemplate />
+  </Transition>
+  <ReuseTemplate v-else />
 </template>
+
+<style lang="css" scoped>
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transition:
+    opacity 0.15s ease-out,
+    transform 0.15s ease-out;
+  will-change: transform, opacity;
+}
+
+.slide-fade-enter-from {
+  transform: translateY(20px) translateX(-50%);
+  opacity: 0;
+}
+
+.slide-fade-leave-to {
+  transform: translateY(20px) translateX(-50%);
+  opacity: 0;
+}
+</style>
