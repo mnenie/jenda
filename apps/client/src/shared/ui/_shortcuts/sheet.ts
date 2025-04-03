209c9d369c8d2435a2431506680ea94ba53b69f4
mnenie <121057011+mneniee@users.noreply.github.com>
2025-04-03 21:06:12 +0300

    chore(ui): update sheet shortcuts

--- apps/client/src/shared/ui/_shortcuts/sheet.ts
+++ apps/client/src/shared/ui/_shortcuts/sheet.ts
@@ -1,12 +1,15 @@
 type SheetPrefix = 'sheet'
 
 export const staticSheet: Record<`${SheetPrefix}-${string}` | SheetPrefix, string> = {
-  'sheet': 'fixed z-99 gap-4 bg-white p-6 focus:(outline-none ring-0) shadow-none transition ease-in-out dark:bg-neutral-800',
-  'sheet-overlay': 'fixed inset-0 z-50 bg-black/80',
+  'sheet': 'fixed z-999 gap-4 bg-white p-6 focus:(outline-none ring-0) shadow-none transition ease-in-out dark:bg-neutral-800',
+  'sheet-overlay': 'fixed inset-0 z-999 bg-black/80',
+  'sheet-portal-overlay': 'sheet-overlay bg-transparent',
   'sheet-description': 'text-sm text-neutral-500 dark:text-neutral-400',
   'sheet-title': 'text-xl 2xl:text-lg font-semibold text-neutral-950 dark:text-neutral-50',
   'sheet-footer': 'flex flex-col-reverse sm:flex-row sm:justify-end sm:gap-x-2',
   'sheet-header': 'flex flex-col gap-y-2 text-center sm:text-left',
+  'sheet-custom-header': 'dialog-custom-header py-3',
+  'sheet-close': 'dialog-close',
 
   'sheet-right': 'inset-y-0 right-0 h-full w-3/4 border-l border-layout',
   'sheet-left': 'inset-y-0 left-0 h-full w-3/4 border-r border-layout',
--- apps/client/src/shared/ui/sheet/UiSheetHeader.vue
+++ apps/client/src/shared/ui/sheet/UiSheetHeader.vue
@@ -1,19 +1,32 @@
 <script setup lang="ts">
+import { UiSheetClose } from '.'
 import type { HTMLAttributes } from 'vue'
 import { cn } from '@/shared/libs/shadcn/utils'
 
-const props = defineProps<{ class?: HTMLAttributes['class'] }>()
+const { custom = false, ...props } = defineProps<{
+  class?: HTMLAttributes['class']
+  custom?: boolean
+}>()
 </script>
 
 <template>
   <div
     :class="
       cn(
-        'sheet-header',
+        custom ? 'sheet-custom-header' : 'sheet-header',
         props.class,
       )
     "
   >
     <slot />
+    <div v-if="custom" class="flex items-center gap-2">
+      <slot name="actions" />
+      <UiSheetClose
+        class="sheet-close"
+      >
+        <div i-lucide-x class="w-4 h-4" />
+        <span class="sr-only">Close</span>
+      </UiSheetClose>
+    </div>
   </div>
 </template>
