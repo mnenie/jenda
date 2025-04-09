f874ca083633ae4c3859fc25bb27672f1e6cdaae
mnenie <121057011+mneniee@users.noreply.github.com>
2025-04-09 18:26:05 +0300

    feat: add get board by id

---
+++ packages/mock-config-server/constants/board.ts
@@ -0,0 +1,9 @@
+export const board_data = {
+  name: 'test title',
+  columns: [],
+  status: 'todo',
+  labels: [],
+  users: [],
+  color: '#fcba03',
+  estimate: 0,
+}
--- packages/mock-config-server/constants/task.ts
+++ packages/mock-config-server/constants/task.ts
@@ -1,20 +1,8 @@
 export const task_data = {
-  _id: '1',
-  title: 'Add i18n',
-  description: 'Add i18n to the app',
+  title: 'test title',
+  description: 'test description',
   users: [],
-  commentsGroup: [{
-    date: new Date(2025, 3, 8),
-    comments: [{
-      _id: '1',
-      message: 'test message',
-      user: {
-        _id: '1',
-        email: 'alex@test.com',
-        photoUrl: 'https://avatars.githubusercontent.com/u/121057011?v=4',
-      },
-    }],
-  }],
+  commentsGroup: [],
   creator: {
     _id: '1',
     email: 'alex@test.com',
