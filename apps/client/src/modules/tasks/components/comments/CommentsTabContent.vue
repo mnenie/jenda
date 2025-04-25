002a4e7f15eaac5933542569bf08a24be7e88cfe
mnenie <121057011+mneniee@users.noreply.github.com>
2025-04-25 23:36:22 +0300

    refactor: tabs contents

---
+++ apps/client/src/modules/tasks/components/comments/CommentsTabContent.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useCommentsQuery } from '../../queries/comments'
+import CommentsList from './CommentsList.vue'
+import InlineCommentEditor from './InlineCommentEditor.vue'
+import EmptyComments from './EmptyComments.vue'
+
+const { data: comments } = useCommentsQuery()
+</script>
+
+<template>
+  <div class="h-full w-full overflow-y-hidden bg-transparent">
+    <CommentsList v-if="comments?.length" />
+    <EmptyComments v-else />
+  </div>
+  <InlineCommentEditor />
+</template>
---
+++ apps/client/src/modules/tasks/components/desc-editor/DescriptionTabContent.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import DescriptionEditor from './DescriptionEditor.vue'
+import EditedBy from './EditedBy.vue'
+</script>
+
+<template>
+  <div class="h-full w-full bg-transparent">
+    <DescriptionEditor v-slot="{ task }">
+      <EditedBy :task />
+    </DescriptionEditor>
+  </div>
+</template>
