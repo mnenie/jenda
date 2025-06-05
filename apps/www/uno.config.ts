f59ce2b50affc817d6e72f27bb6bca771a87fc8b
mnenie <1alexpeshkov@gmail.com>
2025-06-05 23:22:13 +0300

    chore: update uno config

--- apps/www/uno.config.ts
+++ apps/www/uno.config.ts
@@ -1,11 +1,13 @@
 import { defaultConfig } from '@una-ui/nuxt/una.config'
 
 export default defaultConfig({
-  shortcuts: {
-    'text-default': 'text-base 2xl:text-sm',
-    'text-small': 'text-sm 2xl:text-xs',
-    'bg-default': 'bg-white dark:bg-#262626',
-    'border-default': 'border-neutral-200 dark:border-neutral-800',
-    'navigation-menu-trigger-extended': 'h-8 px-2 text-neutral-500 fw600 hover:text-neutral-900 dark:text-neutral-100 dark:hover:text-neutral-200',
-  },
+  shortcuts: [
+    {
+      'text-default': 'text-base 2xl:text-sm',
+      'text-small': 'text-sm 2xl:text-xs',
+      'bg-default': 'bg-white dark:bg-#262626',
+      'border-default': 'border-neutral-200 dark:border-neutral-800',
+      'navigation-menu-trigger-extended': 'h-8 px-2 text-neutral-500 fw600 hover:text-neutral-900 dark:text-neutral-100 dark:hover:text-neutral-200',
+    },
+  ],
 })
